0d5770eb6878077053d4f146119a2955205bfb86
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2024-06-16 23:36:42 +0600

    Added circle shader

--- Assets/Scripts/AssignTexture.cs
+++ Assets/Scripts/AssignTexture.cs
@@ -13,26 +13,27 @@ public class AssignTexture : MonoBehaviour
     
     private static readonly int Result = Shader.PropertyToID("Result");
     private static readonly int MainTex = Shader.PropertyToID("_MainTex");
+    private static readonly int TexResolution = Shader.PropertyToID("texResolution");
 
-    private void OnEnable()
-    {
-        inputActionReference.action.performed += OnActionPerformed;
-        inputActionReference.action.Enable();
-    }
-
-    private void OnDisable()
-    {
-        inputActionReference.action.performed -= OnActionPerformed;
-        inputActionReference.action.Disable();
-    }
-
-    private void OnActionPerformed(InputAction.CallbackContext callbackContext)
-    {
-        if (callbackContext.performed)
-        {
-            DispatchShader(texResolution/8, texResolution/8);
-        }
-    }
+    // private void OnEnable()
+    // {
+    //     inputActionReference.action.performed += OnActionPerformed;
+    //     inputActionReference.action.Enable();
+    // }
+    //
+    // private void OnDisable()
+    // {
+    //     inputActionReference.action.performed -= OnActionPerformed;
+    //     inputActionReference.action.Disable();
+    // }
+    //
+    // private void OnActionPerformed(InputAction.CallbackContext callbackContext)
+    // {
+    //     if (callbackContext.performed)
+    //     {
+    //         DispatchShader(texResolution/8, texResolution/8);
+    //     }
+    // }
 
     private void Start()
     {
@@ -49,17 +50,19 @@ public class AssignTexture : MonoBehaviour
 
     private void InitShader()
     {
-        _kernelHandle = shader.FindKernel("CSMain");
+        // _kernelHandle = shader.FindKernel("SolidRed");
+        // _kernelHandle = shader.FindKernel("SolidYellow");
+        // _kernelHandle = shader.FindKernel("SplitScreen");
+        _kernelHandle = shader.FindKernel("Circle");
         shader.SetTexture(_kernelHandle, Result, _outputTexture);
+        shader.SetInt(TexResolution, texResolution);
         _renderer.material.SetTexture(MainTex, _outputTexture);
         
-        DispatchShader(texResolution/16, texResolution/16);
+        DispatchShader(texResolution/8, texResolution/8);
     }
 
     private void DispatchShader(int x, int y)
     {
         shader.Dispatch(_kernelHandle, x, y, 1); 
-    }
-    
-    
+    } 
 }
